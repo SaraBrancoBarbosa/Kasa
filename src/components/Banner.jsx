92c93a9df15f67fc4b80d4842b87f4535ff57a74
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-01-02 15:05:39 +0100

    Banner update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,7 @@
 function Banner() {
-	return <h1>Chez vous, <span className="mobile-break-line">partout et ailleurs</span></h1>
+	return (
+		<h1>Chez vous, <span className="mobile-break-line">partout et ailleurs</span></h1>
+	)
 }
 
 export default Banner
