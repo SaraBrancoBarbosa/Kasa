897b44efa14c6cc9a5ca9c57df123ab3df78f5cf
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-01-16 17:07:26 +0100

    Changes in css names + public path + children props (node)

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -2,40 +2,39 @@ import { useState, useRef } from "react"
 import "./collapse.css"
 import PropTypes from "prop-types"
 
-function Collapse({title, text}) {
+function Collapse({title, children}) {
     const [visible, setVisible] = useState(false)
 
     const parentRef = useRef()
-
-
+    
     const toggle = () => {
         setVisible(!visible)
     }
 
     return (
+        <div className="collapse">
             <div className="collapse-content">
-                <div className="collapse">
-                    <h2>{title}</h2>
-                    <img src="public/assets/icons/chevron.svg"
-                        onClick={toggle}
-                        className={visible ? "chevron-down" : "chevron-up"}
-                        alt="Afficher ou cacher le texte."
-                    />
-                </div>
-                <div 
-                    className="text-parent" 
-                    ref={parentRef} 
-                    style={ visible ? { height: parentRef.current.scrollHeight + "px" } : {  height: "0px" } }
-                >
-                    <div className="text">{text}</div>
-                </div>
-            </div> 
+                <h2>{title}</h2>
+                <img src="/public/assets/icons/chevron.svg"
+                    onClick={toggle}
+                    className={visible ? "chevron-down" : "chevron-up"}
+                    alt="Afficher ou cacher le texte."
+                />
+            </div>
+            <div 
+                className="text-parent" 
+                ref={parentRef} 
+                style={ visible ? { height: parentRef.current.scrollHeight + "px" } : {  height: "0px" } }
+            >
+                <div className="text">{children}</div>
+            </div>
+        </div> 
     )
 }
 
 Collapse.propTypes = {
     title: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
+    children: PropTypes.node,
 }
 
 export default Collapse
