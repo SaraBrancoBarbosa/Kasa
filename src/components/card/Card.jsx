1e4887b159095b22eb4cd4daf062aaed6e281b1f
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-01-17 16:06:18 +0100

    Component Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,22 @@
+import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 import "./card.css"
 
-function Card(id, title, cover) {
+function Card({id, title, cover}) {
     return (
 		<Link to={`./rental/${id}`}>
 			<figure className="card">
 				<img src={cover} alt="Image du logement" />
-				<figcaption> {title} </figcaption>
+				<figcaption>{title}</figcaption>
 			</figure>
 		</Link>
 	)
+}
+
+Card.propTypes = {
+	id: PropTypes.string,
+	title: PropTypes.string,
+	cover: PropTypes.string,
 }
 
 export default Card
