b8044cab5436ee9da54202625199172118b25c07
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-01-17 16:58:42 +0100

    To hide the arrows and indicator when only 1 picture

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,14 +3,12 @@ import { useState } from "react"
 import "./carousel.css"
 
 const Carousel = ({ data }) => {
-
     const [slide, setSlide] = useState(0)
 
-    if (!data) {
-        return "Aucune location trouvée."
-    }
-
     const totalPictures = data.pictures.length
+
+    // To hide the arrows and indicator when there is only one picture
+    const singlePicture = totalPictures === 1
 
     const nextSlide = () => {
         setSlide(slide === totalPictures - 1 ? 0 : slide + 1)
@@ -23,15 +21,15 @@ const Carousel = ({ data }) => {
     return ( 
         <div className="carousel">
 
-            <img src="../../../public/assets/icons/Arrow-left.svg" className="arrow arrow-left" onClick={previousSlide} alt="Image précédente" />
+            <img src="/public/assets/icons/Arrow-left.svg" className={`arrow arrow-left ${singlePicture ? "hidden" : ""}`} onClick={previousSlide} alt="Image précédente" />
 
             {data.pictures.map((picture, idx) => {
                 return <img src={picture} key={`picture-${idx}`} className={slide === idx ? "slide" : "slide slide-hidden"}/>
             })}
             
-            <img src="../../../public/assets/icons/Arrow-left.svg" className="arrow arrow-right" onClick={nextSlide} alt="Image suivante" />
+            <img src="/public/assets/icons/Arrow-left.svg" className={`arrow arrow-right ${singlePicture ? "hidden" : ""}`} onClick={nextSlide} alt="Image suivante" />
 
-            <span className="indicator">{slide + 1}/{totalPictures}</span>
+            <span className={`indicator ${singlePicture ? "hidden" : ""}`}>{slide + 1}/{totalPictures}</span>
 
         </div>
     )
