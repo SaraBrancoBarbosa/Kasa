a94136d2d52c6fe2a38a87721b6cc3a14277ca54
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-01-20 10:46:41 +0100

    Add useLocation + better management of code and message system

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,12 +1,16 @@
-// To display the 404 error page
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from "react-router-dom"
 import "./error.css"
 
 function Error() {
+
+    const location = useLocation()
+    const state = location ?.state
+    const {code, message} = state ?? {code: "404", message: undefined}
+    console.log(code, message)
     return (
         <div className="error">
-            <h1>404</h1>
-            <h2>Oups ! La page que vous demandez n&apos;existe pas.</h2>
+            <h1>{code ?? "404"}</h1>
+            <h2>{message ?? (code==="404" ? <>Oups ! La page que vous demandez n&apos;existe pas.</> : "")}</h2>
             <Link to="/">
                 <h3>Retourner sur la page d&apos;accueil</h3>
             </Link>
